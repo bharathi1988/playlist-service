381277da7c9fd97add82604ad6d7b914233eaa0b
m_232294 <bharathi1988@gmail.com>
2021-01-30 12:04:32 -0600

    added API

--- src/main/java/com/galvanize/playlistservice/controller/AddSongController.java
+++ src/main/java/com/galvanize/playlistservice/controller/AddSongController.java
@@ -2,12 +2,13 @@ package com.galvanize.playlistservice.controller;
 
 import com.galvanize.playlistservice.entities.Playlist;
 import com.galvanize.playlistservice.service.AddSongService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags="add songs")
 public class AddSongController {
 
     private AddSongService addSongService;
