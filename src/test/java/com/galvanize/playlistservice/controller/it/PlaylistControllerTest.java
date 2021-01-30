ca727a9d036eb73ef7493191d0c2be3f7a6a870f
uthircloudnative <uthircloudnative@gmail.com>
2021-01-30 15:47:39 -0500

    Create playlist error handling impl with test case

--- src/test/java/com/galvanize/playlistservice/controller/it/PlaylistControllerTest.java
+++ src/test/java/com/galvanize/playlistservice/controller/it/PlaylistControllerTest.java
@@ -1,5 +1,6 @@
 package com.galvanize.playlistservice.controller.it;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.galvanize.playlistservice.entities.Playlist;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.server.ResponseStatusException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -37,8 +42,20 @@ public class PlaylistControllerTest {
 
     }
 
+    @Test
+    void createPlayList_whenReturnsErrorMsg400() throws Exception {
+        Playlist pl1 = new Playlist("");
 
+        ResponseStatusException actual = (ResponseStatusException) mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/playlist")
+                               .contentType(MediaType.APPLICATION_JSON)
+                               .content(mapper.writeValueAsString(pl1)))
+                               .andExpect(status().isBadRequest())
+                               .andExpect(result -> assertThat(result.getResolvedException() instanceof ResponseStatusException))
+                               .andReturn().getResolvedException();
 
+        assertEquals("PlaylistName Required",actual.getReason());
+
+    }
 
 
 }
