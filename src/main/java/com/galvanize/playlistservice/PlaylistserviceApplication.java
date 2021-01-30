102a8aeebe0b8bad2029060b06ae2f543e3f12a6
Ofeoritse nanna <nannaofe20@gmail.com>
2021-01-30 09:34:43 -0600

    initial commit

---
+++ src/main/java/com/galvanize/playlistservice/PlaylistserviceApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.playlistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistserviceApplication.class, args);
+	}
+
+}
