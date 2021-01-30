f173a5735d8d96cb5837974aa966353f53509882
Ofeoritse nanna <nannaofe20@gmail.com>
2021-01-30 12:56:10 -0600

    fixing the add song test case

--- src/test/java/com/galvanize/playlistservice/controller/it/AddSongsIntegrationTests.java
+++ src/test/java/com/galvanize/playlistservice/controller/it/AddSongsIntegrationTests.java
@@ -1,6 +1,7 @@
 package com.galvanize.playlistservice.controller.it;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.playlistservice.entities.Playlist;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -27,6 +29,16 @@ public class AddSongsIntegrationTests {
 
     @Test
     public void addSongsToEmptyPlaylistTest() throws Exception{
+        Playlist pl1 = new Playlist("1950 Hits");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/playlist")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(pl1)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.playlistName").value("1950 Hits"))
+                .andExpect(jsonPath("$.songs").isEmpty());
+
         mockMvc.perform(patch("/api/v1/playlist/1/1")
                         .contentType(MediaType.APPLICATION_JSON)
                         )
